85e4b9fbd2ccb35234db8e49836840bc4068c5c3
Napat Makjaroen <89371761+Patboh@users.noreply.github.com>
2024-04-23 14:47:13 +0700

    File errors (for count error)

---
+++ warehouse-project/errors.php
@@ -0,0 +1,10 @@
+<!-- เก็บ errors -->
+<?php $errors = array(); ?>
+
+<?php if (count($errors) > 0) : ?>
+    <div class="error">
+        <?php foreach($errors as $errors) : ?>
+            <p><?php echo $errors ?></p>
+        <?php endforeach ?>
+    </div>
+<?php endif ?>
