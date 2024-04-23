b5e66aafb3717ed5c2edfddd6369e6bcf73594c8
Napat Makjaroen <89371761+Patboh@users.noreply.github.com>
2024-04-23 14:45:29 +0700

    Add Code Sql "insert form to db"

--- warehouse-project/db_checkin.php
+++ warehouse-project/db_checkin.php
@@ -7,9 +7,26 @@ if (!isset($_SESSION['username'])) {
 }
 
 include "db_connect.php";
-$storage_ID = $_POST['storage_ID'];
-$storage_productID = $_POST['storage_productID'];
-$storage_Name = $_POST['storage_Name'];
-$storage_Status = $_POST['storage_Status'];
 
-$sql = "UPDATE storage SET storage_Name='$storage_Name',storage_Status='$storage_Status' WHERE storage_ID='$storage_ID'";
+if (isset($_POST['checkin-submit'])) {
+    $product_ID = mysqli_real_escape_string($conn, $_POST['Product-id']);
+    $product_name = mysqli_real_escape_string($conn, $_POST['product-name']);
+
+    $sql = "INSERT INTO product (productID, productName) VALUES ('$product_ID','$product_name')";
+    mysqli_query($conn, $sql);
+
+    header('location: checkin.php');
+    $_SESSION['success'] = "Check in successfully!";
+
+    // if (){ ถ้ามีเงื่อไขcheck ห้ามซ้ำ 
+    //     header('Location: checkin.php');
+    // $_SESSION['errors'] = "Check in Unsuccessfully!";
+    // }
+}
+
+// $storage_ID = $_POST['storage_ID'];
+// $storage_productID = $_POST['storage_productID'];
+// $storage_Name = $_POST['storage_Name'];
+// $storage_Status = $_POST['storage_Status'];
+
+// $sql = "UPDATE storage SET storage_Name='$storage_Name',storage_Status='$storage_Status' WHERE storage_ID='$storage_ID'";
